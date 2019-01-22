3e2f4442b4e69763b312bd5fa11bcf1e98cbfc3f
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-01-21 21:19:14 -0500

    Added sort by name

--- src/project1/States.py
+++ src/project1/States.py
@@ -1,6 +1,7 @@
 class States:
     def __init__(self, states):
         self._states = states
+        self._sorted_by_name = False
 
     def report(self):
         print "--------------------------------------------------------------------------------------"
@@ -15,3 +16,28 @@ class States:
                 state.name, state.capital, state.abbr,
                 "{:,}".format(state.population), state.region, state.seats
             )
+
+    def sort_by_name(self):
+        self._quicksort(self._states, 0, len(self._states) - 1)
+        self._sorted_by_name = True
+
+    def _quicksort(self, array, lo, hi):
+        if lo < hi:
+            p = self._partition(array, lo, hi)
+            self._quicksort(array, lo, p - 1)
+            self._quicksort(array, p + 1, hi)
+
+    def _partition(self, array, lo, hi):
+        pivot = array[hi]
+        i = lo
+        for j in range(lo, hi):
+            if array[j].name < pivot.name:
+                self._swap(array, i, j)
+                i += 1
+        self._swap(array, i, hi)
+        return i
+
+    def _swap(self, array, i, j):
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
