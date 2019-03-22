a67f47069bceec8ebfe477056c2aaaeacacbbe4e
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-03-22 14:15:28 -0400

    added penalty calculations

--- src/project3/gui.py
+++ src/project3/gui.py
@@ -1,9 +1,11 @@
+import random
 import tkFileDialog as filedialog
 from Tkinter import *
 from subprocess import Popen, PIPE
 
 from attributes import Attributes, Attribute
 from constraints import Constraint
+from preference import Preference
 
 
 class UserPreferencesGUI:
@@ -58,16 +60,16 @@ class UserPreferencesGUI:
         Label(frame, text="Results", font="Consolas 11 bold").grid(row=0, column=6, sticky=W, padx=10)
         Button(frame, text="Generate", command=self.generate_results).grid(row=0, column=7, padx=10, pady=10, sticky=E)
 
-        self._attributes = Text(frame, width=40, borderwidth=2, relief=SOLID)
+        self._attributes = Text(frame, width=50, borderwidth=2, relief=SOLID)
         self._attributes.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky=W + E)
 
-        self._constraints = Text(frame, width=40, borderwidth=2, relief=SOLID)
+        self._constraints = Text(frame, width=50, borderwidth=2, relief=SOLID)
         self._constraints.grid(row=1, column=2, columnspan=2, padx=10, pady=10, sticky=W + E)
 
-        self._preferences = Text(frame, width=40, borderwidth=2, relief=SOLID)
+        self._preferences = Text(frame, width=50, borderwidth=2, relief=SOLID)
         self._preferences.grid(row=1, column=4, columnspan=2, padx=10, pady=10, sticky=W + E)
 
-        self._results = Text(frame, width=40, borderwidth=2, relief=SOLID)
+        self._results = Text(frame, width=50, borderwidth=2, relief=SOLID)
         self._results.grid(row=1, column=6, columnspan=2, padx=10, pady=10)
 
         frame.pack()
@@ -77,45 +79,102 @@ class UserPreferencesGUI:
         self.set_results("")
         print "generating results"
         try:
-            attributes = Attributes()
+            # -------------------------------------------------------------------------------------------------- #
             print "parsing attributes"
+            attributes = Attributes()
             for s in self.get_attributes().split("\n"):
                 att = Attribute.parse(s)
                 if att is not None:
                     attributes.add(att)
             if attributes.length() == 0:
                 raise Exception("No valid Attributes found")
-            constraints = []
+            # -------------------------------------------------------------------------------------------------- #
             print "parsing constraints"
+            constraints = []
             for s in self.get_constraints().split("\n"):
-                c = Constraint(s)
-                constraints.append(c)
+                constraints.append(Constraint(s))
             clasp = "p cnf %s %s\n" % (attributes.length(), len(constraints))
             for c in constraints:
                 parse = c.parse_to_clasp(attributes)
-                if parse != "0":
+                if parse is not None and parse != "0":
                     clasp = clasp + parse + "\n"
+            # -------------------------------------------------------------------------------------------------- #
             print "writing to file"
             self.write_to_file("clasp_in.txt", clasp)
             print "running clasp"
             results = self.run_clasp("clasp_in.txt")
-            self.append_results("{:-^40}\n".format(""))
-            self.append_results("{:^40}".format("FEASIBLE OBJECTS"))
-            self.append_results("\n{:-^40}\n".format(""))
-            self.append_results(self.format_clasp_results(results, attributes))
+            clasp_objects = self.get_objects_from_clasp(results)
+            # -------------------------------------------------------------------------------------------------- #
+            print "parsing preferences"
+            preferences = []
+            for p in self.get_preferences().split("\n"):
+                p = p.strip()
+                if p != "":
+                    match = re.match("(?P<preference>[a-zA-Z\s]+)\s*,\s*(?P<penalty>\d+)", p)
+                    if match is None:
+                        raise Exception("Invalid Preference format")
+                    pref = match.group("preference")
+                    penalty = match.group("penalty")
+                    preferences.append(Preference(pref, int(penalty)))
+            # -------------------------------------------------------------------------------------------------- #
+            print "calculating penalties"
+            object_penalties = Preference.calculate_penalties(clasp_objects, attributes, preferences)
+            exemplification = random.sample(object_penalties, 2)
+            # -------------------------------------------------------------------------------------------------- #
+            print "writing results"
+            self.append_results("{:-^50}\n".format(""))
+            self.append_results("{:^50}".format("FEASIBLE OBJECTS"))
+            self.append_results("\n{:-^50}\n".format(""))
+            self.append_results("There are " + str(len(clasp_objects)) + " feasible objects")
+
+            if len(clasp_objects) == 0:
+                return
+
+            self.append_results("\n\n{:-^50}\n".format(""))
+            self.append_results("{:^50}\n".format("EXEMPLIFICATION"))
+            self.append_results("{:-^50}\n".format(""))
+            self.append_results("[Object Values], Penalty Value\n")
+            self.append_results("O1 = [" + attributes.from_clasp(exemplification[0][0]) + "], "
+                                + str(exemplification[0][1]) + "\n")
+            self.append_results("O2 = [" + attributes.from_clasp(exemplification[1][0]) + "], "
+                                + str(exemplification[1][1]) + "\n")
+            if exemplification[0][1] > exemplification[1][1]:
+                self.append_results("O2 is strictly preferred to O1")
+            elif exemplification[0][1] < exemplification[1][1]:
+                self.append_results("O1 is strictly preferred to O2 ")
+            else:
+                self.append_results("O1 is equally preferred to O2")
+
+            self.append_results("\n\n{:-^50}\n".format(""))
+            self.append_results("{:^50}\n".format("OPTIMAL OBJECTS"))
+            self.append_results("{:-^50}\n".format(""))
+            object_penalties.sort(key=lambda e: e[1])
+            best = object_penalties[0][1]
+            for o in object_penalties:
+                if o[1] == best:
+                    self.append_results("[%s], %s\n" % (attributes.from_clasp(o[0]), o[1]))
         except Exception as ex:
             self.set_results(ex.message)
 
-    def format_clasp_results(self, clasp, attributes):
+    def format_clasp_results(self, objects, attributes, preferences):
+        if len(objects) == 0:
+            return "No Feasible Objects"
         results = ""
         i = 1
+        for s in objects:
+            s = s.strip()
+            results = results + str(i) + ". (" \
+                      + str(Preference.calculate_penalty(s, attributes, preferences)) + ") " \
+                      + attributes.from_clasp(s) + "\n"
+            i = i + 1
+        return results
+
+    def get_objects_from_clasp(self, clasp):
+        objects = []
         for s in clasp.split("\n"):
             if s.startswith("v"):
-                results = results + str(i) + ". " + attributes.from_clasp(s[2:]) + "\n"
-                i = i + 1
-        if results == "":
-            results = "No Feasible Objects"
-        return results
+                objects.append(s[2:])
+        return objects
 
     def write_to_file(self, filename, value):
         with open(filename, "w") as f:
--- src/project3/preference.py
+++ src/project3/preference.py
@@ -20,3 +20,30 @@ class Preference:
             clause = Clause(c)
             clasp_clauses.append(clause.parse_clause_to_clasp(attributes))
         return clasp_clauses
+
+    @staticmethod
+    def calculate_penalties(objects, attributes, preferences):
+        penalties = []
+        for o in objects:
+            penalties.append([o, Preference.calculate_penalty(o, attributes, preferences)])
+        return penalties
+
+    @staticmethod
+    def calculate_penalty(feasible_object, attributes, preferences):
+        penalty = 0
+        for p in preferences:
+            clasp = p.to_clasp_clauses(attributes)
+            if not Preference.__object_is_preferred(feasible_object, clasp):
+                penalty = penalty + p.penalty()
+        return penalty
+
+    @staticmethod
+    def __object_is_preferred(feasible_object, preference):
+        for clause in preference:
+            clause = clause[:-2]
+            for item in clause.split(" "):
+                if item in feasible_object and "-" + item not in feasible_object:
+                    break
+            else:
+                return False
+        return True
