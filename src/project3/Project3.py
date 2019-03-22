f5e050e9ec1a5720b482d5101e311d797e2756a5
Kyle Shappley <Kyle.Shappley@gmail.com>
2019-03-21 20:08:44 -0400

    added clasp results generation

--- src/project3/Project3.py
+++ src/project3/Project3.py
@@ -1,16 +1,14 @@
 """
 Stephen Shappley
 N01184718
-CAP4630 - Spring 2019
+CAP4630 - Project 3 - Spring 2019
 """
 
-from subprocess import Popen, PIPE
+from gui import UserPreferencesGUI
 
 
 def main():
-    process = Popen(["clasp", "0", "cnf_test.txt"], stdout=PIPE, stderr=PIPE)
-    out, err = process.communicate()
-    print(out)
+    UserPreferencesGUI()
 
 
 main()
--- src/project3/gui.py
+++ src/project3/gui.py
@@ -1,5 +1,9 @@
-from Tkinter import *
 import tkFileDialog as filedialog
+from Tkinter import *
+from subprocess import Popen, PIPE
+
+from attributes import Attributes, Attribute
+from constraints import Constraint
 
 
 class UserPreferencesGUI:
@@ -26,6 +30,10 @@ class UserPreferencesGUI:
     def get_constraints(self):
         return self._constraints.get(1.0, END)
 
+    def set_results(self, value):
+        self._results.delete(1.0, END)
+        self._results.insert(END, value)
+
     def gui(self):
         root = Tk()
         root.title("CAP4630 Project 3: User Preferences")
@@ -45,7 +53,7 @@ class UserPreferencesGUI:
             .grid(row=0, column=5, sticky=E, padx=10)
 
         Label(frame, text="Results", font="Consolas 11 bold").grid(row=0, column=6, sticky=W, padx=10)
-        Button(frame, text="Generate").grid(row=0, column=7, padx=10, pady=10, sticky=E)
+        Button(frame, text="Generate", command=self.generate_results).grid(row=0, column=7, padx=10, pady=10, sticky=E)
 
         self._attributes = Text(frame, width=35, borderwidth=2, relief=SOLID)
         self._attributes.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky=W + E)
@@ -61,3 +69,46 @@ class UserPreferencesGUI:
 
         frame.pack()
         root.mainloop()
+
+    def generate_results(self):
+        print "generating results"
+        try:
+            attributes = Attributes()
+            print "parsing attributes"
+            for s in self.get_attributes().split("\n"):
+                att = Attribute.parse(s)
+                if att is not None:
+                    attributes.add(att)
+            if attributes.length() == 0:
+                raise Exception("No valid Attributes found")
+            constraints = []
+            print "parsing constraints"
+            for s in self.get_constraints().split("\n"):
+                c = Constraint(s)
+                constraints.append(c)
+            clasp = "p cnf %s %s\n" % (attributes.length(), len(constraints))
+            print clasp
+            for c in constraints:
+                parse = c.parse_to_clasp(attributes)
+                if parse != "0":
+                    clasp = clasp + parse + "\n"
+            print "writing to file"
+            self.write_to_file("clasp_in.txt", clasp)
+            print "running clasp"
+            results = self.run_clasp("clasp_in.txt")
+            print results
+            self.set_results(results)
+        except Exception as ex:
+            self.set_results(ex.message)
+
+    def write_to_file(self, filename, value):
+        with open(filename, "w") as f:
+            f.write(value)
+
+    def run_clasp(self, filename):
+        process = Popen(["clasp", "0", filename], stdout=PIPE, stderr=PIPE)
+        out, err = process.communicate()
+        return out
+
+
+UserPreferencesGUI()
